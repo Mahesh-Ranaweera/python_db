4b4abff2f6e4a9ca6a56e2b31222526f72b27ac1
Mahesh-Ranaweera <mahesh.ranaweerakaluarachchige@uoit.net>
2017-01-05 20:26:48 -0500

    Added flask

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+name = ['Mahesh','Ranaweera','Sachin','Sachith']
 
 #homepage
 @app.route('/')
@@ -11,6 +13,16 @@ def index():
 @app.route('/profile/<name>')
 def profile(name):
     return render_template("profile.html", name=name)
+
+#register
+@app.route('/register')
+def register():
+    return render_template("register.html", selected='register', names=name)
+
+@app.route('/register2', methods=['POST'])
+def reply():
+    name2=request.form['name2']
+    return render_template("register2.html", finalname=name2)
 
 
 if __name__ == "__main__":
